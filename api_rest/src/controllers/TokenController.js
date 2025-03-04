7c395cae17e93172830e11da9436809976c4e9cd
Daniel Gaiguer <gaiguer2019@gmail.com>
2025-03-04 00:29:53 -0300

    token upgrade

--- api_rest/src/controllers/TokenController.js
+++ api_rest/src/controllers/TokenController.js
@@ -1,11 +1,37 @@
 import User from '../models/User';
+import jwt from 'jsonwebtoken'
 
 class TokenController {
   async store(req, res) {
     const { email = '', password = '' } = req.body;
 
+    if(!email || !password) {
+      return res.status(401).json({
+        errors: ['Credenciais Invalidas']
+      });
+    }
+
     const user = await User.findOne({ where: { email } });
-    res.json('OK');
+
+    if(!user) {
+      return res.status(401).json({
+        errors: ['Usuario nao existe']
+      });
+    }
+
+    if(!(await user.passIsValid(password))){
+      return res.status(401).json({
+        errors: ['Senha Invalida']
+      });
+    }
+
+    const { id } = user;
+    const token = jwt.sign({ id, email }, process.env.TOKEN_SECRET, {
+      expiresIn: process.env.TOKEN_EXPIRATION,
+    }
+    );
+
+    res.json({ token });
   }
 }
 
