a7b6e3e56ac4bcf155230dc3788eb0b83d336951
Daniel Gaiguer <gaiguer2019@gmail.com>
2025-03-16 19:22:20 -0300

    add css edit and delete

--- REACT/listatarefas/src/components/Main.js
+++ REACT/listatarefas/src/components/Main.js
@@ -4,9 +4,18 @@ import './Main.css';
 //Form
 import { FaPlus } from 'react-icons/fa';
 
+//Tarefas
+import { FaEdit, FaWindowClose } from 'react-icons/fa';
+
+
 export default class Main extends Component{
   state = {
     novaTarefa: '',
+    tarefas: [
+      'Fazer cafe',
+      'beber agua',
+      'estudar'
+    ],
   };
 
 
@@ -17,7 +26,7 @@ export default class Main extends Component{
   }
 
   render(){
-    const { novaTarefa } = this.state;
+    const { novaTarefa, tarefas } = this.state;
     return (
       <div className="main">
         <h1>Lista de Tarefas </h1>
@@ -32,6 +41,18 @@ export default class Main extends Component{
             <FaPlus />
           </button>
         </form>
+
+        <ul className="tarefas">
+          {tarefas.map(tarefa => (
+            <li key={tarefa}>
+              {tarefa}
+              <div>
+              <FaEdit className="edit"/>
+              <FaWindowClose className="delete"/>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     )
   }
