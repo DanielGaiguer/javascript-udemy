46e9195cc57c4b6d234c8adb4e426b05d23b67ef
Daniel Gaiguer <gaiguer2019@gmail.com>
2025-03-16 12:27:24 -0300

    add css input and update input

--- REACT/listatarefas/src/components/Main.js
+++ REACT/listatarefas/src/components/Main.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import './Main.css';
 
+//Form
+import { FaPlus } from 'react-icons/fa';
 
 export default class Main extends Component{
   state = {
@@ -15,13 +17,20 @@ export default class Main extends Component{
   }
 
   render(){
+    const { novaTarefa } = this.state;
     return (
       <div className="main">
         <h1>Lista de Tarefas </h1>
 
-        <form action="#">
-          <input onChange={this.handleChange} type="text"/>
-          <button type="submit">Enviar</button>
+        <form action="#" className="form">
+          <input
+          onChange={this.handleChange}
+          type="text"
+          value={novaTarefa}
+        />
+          <button type="submit">
+            <FaPlus />
+          </button>
         </form>
       </div>
     )
