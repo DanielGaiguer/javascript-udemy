598aef15f72940c3a79c1faa261f15a1c1814111
Daniel Gaiguer <gaiguer2019@gmail.com>
2025-03-17 15:55:49 -0300

    add components

--- REACT/listatarefas/src/components/Main.js
+++ REACT/listatarefas/src/components/Main.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
 import './Main.css';
-
-//Form
-import { FaPlus } from 'react-icons/fa';
+import Tarefas from './Tarefas';
 
 //Tarefas
-import { FaEdit, FaWindowClose } from 'react-icons/fa';
 
+import Form from './Form';
 
 export default class Main extends Component{
   state = {
@@ -88,32 +86,18 @@ export default class Main extends Component{
       <div className="main">
         <h1>Lista de Tarefas </h1>
 
-        <form action="#" className="form" onSubmit={this.handleSubmit}>
-          <input
-          onChange={this.handleChange}
-          type="text"
-          value={novaTarefa}
-        />
-          <button type="submit">
-            <FaPlus />
-          </button>
-        </form>
+      <Form
+      handleSubmit={this.handleSubmit}
+      handleChange={this.handleChange}
+      novaTarefa={novaTarefa}
+      />
 
-        <ul className="tarefas">
-          {tarefas.map((tarefa, index) => (
-            <li key={tarefa}>
-              {tarefa}
-              <span>
-              <FaEdit className="edit"
-              onClick={(e) => this.handleEdit(e, index)}/>
+      <Tarefas
+      tarefas={tarefas}
+      handleEdit={this.handleEdit}
+      handleDelete={this.handleDelete}
+      />
 
-              <FaWindowClose
-              onClick={(e) => this.handleDelete(e, index)}
-              className="delete"/>
-              </span>
-            </li>
-          ))}
-        </ul>
       </div>
     )
   }
