b2d16abbd45114a54e2e5ebbc7e95940be71916b
Daniel Gaiguer <gaiguer2019@gmail.com>
2025-03-17 15:04:42 -0300

    push localstorage

--- REACT/listatarefas/src/components/Main.js
+++ REACT/listatarefas/src/components/Main.js
@@ -14,6 +14,14 @@ export default class Main extends Component{
     tarefas: [],
   };
 
+  componentDidMount(){
+    const tarefas = JSON.parse(localStorage.getItem('tarefas'));
+
+    if (!tarefas) return
+
+    this.setState({ tarefas });
+  }
+
   componentDidUpdate(prevProps, prevState) {
     const { tarefas } = this.state;
     if (tarefas === prevState.tarefas) return;
