7e8cc881337816311e4fdbd3031c691260219c2d
Daniel Gaiguer <gaiguer2019@gmail.com>
2025-03-28 19:05:17 -0300

    teste style of components

---
+++ REACT/react-base/src/pages/Login/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Title } from './styled';
+
+export default function Login(){
+  return (
+  <>
+    <Title isRed = {false}>
+      Login
+      <small>oie</small>
+    </Title>
+    <p>Lorem ipsum dolor sit amet.</p>
+  </>
+  );
+}
