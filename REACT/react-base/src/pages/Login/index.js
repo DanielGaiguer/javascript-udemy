c61c1f6dd87b188d4456181297493a691c237279
Daniel Gaiguer <gaiguer2019@gmail.com>
2025-03-30 10:03:05 -0300

    colors primary

--- REACT/react-base/src/pages/Login/index.js
+++ REACT/react-base/src/pages/Login/index.js
@@ -11,6 +11,7 @@ export default function Login(){
       <small>oie</small>
     </Title>
     <Paragrafo>Lorem ipsum dolor sit amet.</Paragrafo>
+    <button type="button">Salvar</button>
   </Container>
   );
 }
