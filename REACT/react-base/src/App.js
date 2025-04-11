51fa10ba1609dba970833bae30b0f19a6c1a7b20
Daniel Gaiguer <gaiguer2019@gmail.com>
2025-04-11 16:30:29 -0300

    add history

--- REACT/react-base/src/App.js
+++ REACT/react-base/src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
 
+import history from './services/history';
 import GlobalStyle from './styles/GlobalStyles';
 import Header from './components/Header';
 import Routes from './routes';
-import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <Header />
       <Routes />
       <GlobalStyle />
-    </BrowserRouter>
+    </Router>
 );
 }
 
