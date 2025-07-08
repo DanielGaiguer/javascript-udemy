3a16df7a7aaad9891435371cb4be70e5886ee6dc
Daniel Gaiguer <gaiguer2019@gmail.com>
2025-07-08 17:59:48 -0300

    aula 215

--- REACT/react-base/src/store/index.js
+++ REACT/react-base/src/store/index.js
@@ -1,8 +1,21 @@
 import { legacy_createStore as createStore } from 'redux';
 
-const reducer = (state, action) => {
-  console.log(action);
-  return state;
+const initialState = {
+  botaoClicado: false,
+};
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    case 'BOTAO_CLICADO': {
+      const newState = {...state};
+      newState.botaoClicado = !newState.botaoClicado;
+      return newState;
+    }
+
+    default: {
+      return state;
+    }
+  }
 };
 
 const store = createStore(reducer);
