bac9290de125a0cc564d9855113de188c220aaf1
Daniel Gaiguer <gaiguer2019@gmail.com>
2025-07-18 14:18:49 -0300

    documentacao alunos

--- REACT/consumir-api/src/App.js
+++ REACT/consumir-api/src/App.js
@@ -12,13 +12,13 @@ import Routes from './routes';
 
 function App() {
   return (
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <Router history={history}>
-          <Header />
-          <Routes />
-          <GlobalStyle />
-          <ToastContainer autoClose={3000} className="toast-container"/>
+    <Provider store={store}>{/*Vai usar o redux, onde tem varias funcionalidades*/}
+      <PersistGate persistor={persistor}>{/*Vai usar o persistor, que ira guardar o estado da aplicacao */}
+        <Router history={history}>{/*Vai usar o history, para gerenciar os caminhos e o historico dos caminhos do usuario */}
+          <Header />{/*Componente padrao de cabecalho */}
+          <Routes />{/*Todas as possiveos rotas da aplicacao*/}
+          <GlobalStyle />{/*CSS global */}
+          <ToastContainer autoClose={3000} className="toast-container"/>{/*Toast para mensagens com o usuario */}
         </Router>
       </PersistGate>
     </Provider>
--- REACT/consumir-api/src/pages/Alunos/index.js
+++ REACT/consumir-api/src/pages/Alunos/index.js
@@ -8,15 +8,15 @@ import { AlunoContainer, ProfilePicture  }  from "./styled";
 import axios from '../../services/axios';
 
 export default function Alunos(){
-  const [alunos, setAlunos] = useState([]);
+  const [alunos, setAlunos] = useState([]);//Significa que o estado de alunos, vai ser setado o valor pela funcao setAlunos, e o valor padrao desta variavel e um array vazio
 
-  useEffect(() => {
-    async function getData() {
-      const response = await axios.get('/alunos');
-      setAlunos(response.data);
+  useEffect(() => {//Vai ser chamado assim que a pagina for carregada, e um react hook
+    async function getData() {//Funcao para pegar dados da API com o axios
+      const response = await axios.get('/alunos');//O IP da API ja esta setado, e dai ele vai pegar os dados dessa api com /alunos
+      setAlunos(response.data);//Esta funcao vai setar automaticamente os dados do API dentro de alunos com a funcao a setAlunos
     }
 
-    getData();
+    getData();//Chama automaticamente a funcao para pegar dados, assim que todoso o componente da pagina for carregado
   }, []);
 
   return (
@@ -25,8 +25,9 @@ export default function Alunos(){
 
       <AlunoContainer>
         {alunos.map(aluno => (
-          <div key={String(aluno.id)}>
+          <div key={String(aluno.id)}>{/*Vai criar uma div por aluno, com a key do valor do ID do aluno*/}
             <ProfilePicture>
+              {/*get do lodash, vai pegar dentro do aluno, Fotos[0].url, se caso nao ter o valor vai ser false, depois ele vai verificar, se o valor for true ele ira carregar a imagem, se nao ele vai colocar um icone padrao*/}
               { get(aluno, 'Fotos[0].url', false) ? (
                 <img src={aluno.Fotos[0].url} alt="" />
               ) : (
@@ -37,11 +38,11 @@ export default function Alunos(){
             <span>{aluno.nome}</span>
             <span>{aluno.email}</span>
 
-            <Link to={`/aluno/${aluno.id}/edit`}>
+            <Link to={`/aluno/${aluno.id}/edit`}> {/*Link para editar com o ID do aluno  */}
               <FaEdit size={16}/>
             </Link>
 
-            <Link to={`/aluno/${aluno.id}/delete`}>
+            <Link to={`/aluno/${aluno.id}/delete`}>{/*Link para deletar com o ID do aluno */}
               <FaWindowClose size={16}/>
             </Link>
 
