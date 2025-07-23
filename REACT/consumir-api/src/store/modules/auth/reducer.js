58e1cf09b8796dc1f0dd4297047e6ce5643d3ba5
Daniel Gaiguer <gaiguer2019@gmail.com>
2025-07-23 10:47:06 -0300

    Update reducer.js

--- REACT/consumir-api/src/store/modules/auth/reducer.js
+++ REACT/consumir-api/src/store/modules/auth/reducer.js
@@ -1,4 +1,5 @@
 import * as types from '../types';
+import axios from '../../../services/axios';
 
 const initialState = {//Estado inicial e padrao da aplicacao, aqui vai ser setado tudo que posteriormente vai ser salvo no estado
   isLoggedIn: false,
@@ -19,6 +20,7 @@ export default function (state = initialState, action) {//Apos ser chamado no di
     }
 
     case types.LOGIN_FAILURE: {
+      delete axios.defaults.headers.Authorization;
       const newState = { ...initialState };
       return newState;
     }
