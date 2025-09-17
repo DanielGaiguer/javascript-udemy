bf0d3552bbdc133377b4ff0219d18b5bafde95e7
Daniel Gaiguer <gaiguer2019@gmail.com>
2025-09-16 21:24:26 -0300

    start, end, multiline

--- regEx/aula7.js
+++ regEx/aula7.js
@@ -1,5 +1,12 @@
 const { cpfs2 } = require('./base')
 
+// ^ - comeca com
+//$ - termina com
+// [^] - Negacao
+// m - multiline
+
 const cpf = `917.882.779-53`
 
-const cpfRegExp = /(\d{3}\.){2} \d{3}-\d{2}/g
+const cpfRegExp = /^(\d{3}\.){2}\d{3}-\d{2}$/gm
+
+console.log(cpfs2.match(cpfRegExp))
